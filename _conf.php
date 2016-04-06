32e63e21b1da2bb0010c20c3e22321bedfb7bfff
Rodrigo Borth <rodrigo.borth@objectedge.com>
2016-04-06 14:12:35 -0300

    add dependencies

--- _conf.php
+++ _conf.php
@@ -7,9 +7,23 @@
  */
 
 //define contact email(used in forms;
-$contactEmail = "example@example.com";
-
-define("DB_USER", "");
-define("DB_HOST", "");
-define("DB_PASS", "");
-define("DB_BASE", "");
+$configuration = [
+  "database" => [
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'user' => 'testes',
+    'port' => 3306,
+    'password' => 'abc@123',
+    'dbname' => 'ciadastorneiras',
+    'charset' => 'utf8'
+  ],
+  'password' => [
+    'cost' => 8,
+    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
+  ],
+  'form' => [
+    'contact' => 'contact@example.com',
+    'reply' => 'reply@example.com',
+    'sender' => 'no-reply@example.com'
+  ]
+];
--- _init.php
+++ _init.php
@@ -10,29 +10,35 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //define page name
 $pageName = strtolower(
-    isset($_GET['page-name'])
-        ?$_GET['page-name']
-        :"home"
+  isset($_GET['page-name'])
+    ? $_GET['page-name']
+    : "home"
 );
 //define page index (used in pagination)
-$pageIndex = isset($_GET['page-index'])?$_GET['page-index']:1;
+$pageIndex = isset($_GET['page-index']) ? $_GET['page-index'] : 1;
 
 var_dump($pageIndex, $pageName);
 
 //define root dir
 $root = dirname(__FILE__);
 //define models dir
-$modelDir = $root."/model";
-$modelPageDir = $root."/model/page";
-$modelSectionDir = $root."/model/section";
+$modelDir = $root . "/model";
+$modelPageDir = $root . "/model/page";
+$modelSectionDir = $root . "/model/section";
 //define controllers dir
-$controllerDir = $root."/controller";
-$controllerPageDir = $root."/controller/page";
-$controllerSectionDir = $root."/controller/section";
+$controllerDir = $root . "/controller";
+$controllerPageDir = $root . "/controller/page";
+$controllerSectionDir = $root . "/controller/section";
 //define views dir
-$viewDir = $root."/view";
-$viewPageDir = $root."/view/page";
-$viewSectionDir = $root."/view/section";
+$viewDir = $root . "/view";
+$viewPageDir = $root . "/view/page";
+$viewSectionDir = $root . "/view/section";
+
+//require configs
+require $root . "/_conf.php";
+
+//require dbal
+require $root . "/vendor/autoload.php";
 
 //require layout
-require $root."/layout.php";
+require $root . "/layout.php";
